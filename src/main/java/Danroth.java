1c41d0123e78c1ad539b2d2ec682efae69dca8bd
Chris Ward <chris@chrisgward.com>
2011-12-01 21:27:02 +1100

    now to get it to actually run before I start coding

--- src/main/java/Danroth.java
+++ src/main/java/Danroth.java
@@ -1,15 +1,12 @@
-package com.crumbdev.chris.Danroth;
-
 /**
  * Created by IntelliJ IDEA.
  * User: chris
  * Date: 1/12/11
- * Time: 9:16 PM
+ * Time: 9:25 PM
  * To change this template use File | Settings | File Templates.
  */
 public class Danroth {
-    public static void main(String[] args)
-    {
-        System.out.println("herpaderp");
+    public static void main(String[] args) {
+        System.out.println("STOP DERPIN");
     }
 }
